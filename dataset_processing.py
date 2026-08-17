4d700ad24240bd3b5e9ce512d337d27207339b78
Deivy Munoz <dmunoz2460@gmail.com>
2026-08-17 08:47:04 -0400

    gitignore changes

--- dataset_processing.py
+++ dataset_processing.py
@@ -1,13 +1,15 @@
 import cv2 as cv
+import os
 import glob
 import preprocessing_image
 
 for i in range(20):
-    dir = f"dataset/db/{i+1}/*.tiff"
-    # print(dir)
+    dir = f"dataset/{i+1}/D/*.tiff"
+    print(dir)
 
     path = glob.glob(dir)
     for img in path:
+        base_name = os.path.splitext(os.path.basename(img))[0]
         image = cv.imread(img)
         # b,g,r = cv.split(image)
 
@@ -19,9 +21,10 @@ for i in range(20):
             # draw the center of the circle
             cv.circle(image,(circle[0],circle[1]),2,(0,0,255),3)
 
-        cv.imshow(img, image)
+        cv.imwrite(f"dataset/{i+1}/D/{base_name}.jpg", image)
+        # cv.imshow(img, image)
 
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
 
 cv.destroyAllWindows()
